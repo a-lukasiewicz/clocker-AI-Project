cc8758286b5945b9e95ef627b0acf8ad82f30a2c
Michał Zawadzki <zm46704@zut.edu.pl>
2022-01-24 15:36:46 +0100

    dodanie php do mainPage

--- mainPage.php
+++ mainPage.php
@@ -2,8 +2,20 @@
 session_start();
 include("config/connection.php");
 include("config/functions.php");
-$user_data = check_login($con);
-  check_employment($con);
+
+$numberUsers = $con->query("SELECT COUNT('user_id') FROM users");
+$numberWork = $con->query("SELECT SUM('work_time') FROM workrecord");
+//SELECT SUM(work_time) FROM workrecord WHERE MONTH(date) = MONTH(CURDATE()) wybiera godziny z tego miesiaca
+//SELECT SUM(work_time) FROM workrecord WHERE QUARTER(date) = QUARTER(CURDATE()) z tego kwartalu
+//SELECT SUM(work_time) FROM workrecord WHERE DAY(date) = DAY(CURDATE()) z tego dnia
+if(empty($numberUsers))
+{
+  $numberUsers = 0;
+}
+if(empty($numberWork))
+{
+  $numberWork = 0;
+}
 ?>
 
 
@@ -36,16 +48,18 @@ $user_data = check_login($con);
           <li id="numberLi">Liczba godzin przepracowanych</li>
         </ul>
         <ul id="numberUl">
-          <li id="numberLi">Wynosi: <font size="+30">0</font></li>
-          <li id="numberLi">Wynosi: <font size="+10">0</font></li>
+          <li id="numberLi">Wynosi: <font size="+10"><?php echo $numberUsers ?></font></li>
+          <li id="numberLi">Wynosi: <font size="+10"><?php echo $numberWork ?></font></li>
         </ul>
       </div>
       <div id="aboutTitle">
         <h3 id="aboutH1">Na co możesz liczyć?</h3>
       </div>
       <div id="aboutUs">
-        <p id="aboutP">Nasza aplikacja wesprze Twój zespół w organizacji pracy, która częstwo okazuje się problemem. Clocker pomoże Twojej
+        <p id="aboutP">Nasza aplikacja wesprze Twój zespół w organizacji pracy, która często okazuje się problemem. Clocker pomoże Twojej
         firmie, StartUpie, Indie Developerom albo freelancerom w uporządkowaniu czasu pracy jak i śledzenie postępów Twoich pracowników.
+        Wystarczy, że założysz konto w naszym serwisie i wszystkie funkcjonalności będą udostępnione Tobie za darmo! Nie licz na siebie,+
+        licz na NAS!
         </p>
       </div>
   </body>
